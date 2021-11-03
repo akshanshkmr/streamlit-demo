8a456685c73b1a40ce6ce49de41c52e1b3fe3369
Akshansh <akshanshkmr821@gmail.com>
2021-11-03 17:03:06 +0530

    Updated apex charts example

--- src/st_apex_charts_demo.py
+++ src/st_apex_charts_demo.py
@@ -22,48 +22,85 @@ def app(data):
 
     with st.expander('Line Chart Help'):
         st.help(line_chart)
+        st.code('''
+        line_chart('Zoomable Line Chart',df)
+        ''')
     line_chart('Zoomable Line Chart', df)
 
-    with st.expander('Bar chart Help'):
-        st.help(bar_chart)
-    bar_chart('Bar Chart', df)
-    bar_chart('Stacked Bar Chart', df, stacked=True)
-    bar_chart('Horizontal Bar Chart', df, horizontal=True, stacked=True)
+    c1, c2 = st.columns(2)
 
-    with st.expander('Area Chart Help'):
-        st.help(area_chart)
-    area_chart('Area Chart', df)
-    area_chart('Stacked Area Chart', df, stacked=True)
+    with c1:
+        with st.expander('Bar chart Help'):
+            st.help(bar_chart)
+            st.code('''
+            bar_chart('Bar Chart', df)
+            bar_chart('Stacked Bar Chart', df, stacked=True)
+            bar_chart('Horizontal Bar Chart', df, horizontal=True, stacked=True)
+            ''')
+        bar_chart('Bar Chart', df)
+        bar_chart('Stacked Bar Chart', df, stacked=True)
+        bar_chart('Horizontal Bar Chart', df, horizontal=True, stacked=True)
 
-    with st.expander('Pie Chart Help'):
-        st.help(pie_chart)
-    pie_chart('Pie Chart', df)
+        with st.expander('Area Chart Help'):
+            st.help(area_chart)
+            st.code('''
+            area_chart('Area Chart', df)
+            area_chart('Stacked Area Chart', df, stacked=True)
+            ''')
+        area_chart('Area Chart', df)
+        area_chart('Stacked Area Chart', df, stacked=True)
 
-    with st.expander('Donut Chart Help'):
-        st.help(donut_chart)
-    donut_chart('Donut Chart', df)
+        with st.expander('Pie Chart Help'):
+            st.help(pie_chart)
+            st.code('''
+            pie_chart('Pie Chart', df)
+            ''')
+        pie_chart('Pie Chart', df)
 
-    with st.expander('Radar Chart Help'):
-        st.help(radar_chart)
-    radar_chart('Radar Chart', df)
+    with c2:
+        with st.expander('Donut Chart Help'):
+            st.help(donut_chart)
+            st.code('''
+            donut_chart('Donut Chart', df)
+            ''')
+        donut_chart('Donut Chart', df)
 
-    with st.expander('Mixed Chart Help'):
-        st.help(mixed_chart)
-    mixed_chart('Mixed Chart', df, plot_types={
-        'Google':'line',
-        'Apple':'column',
-        'Microsoft':'area'
-    })
+        with st.expander('Radar Chart Help'):
+            st.help(radar_chart)
+            st.code('''
+            radar_chart('Radar Chart', df)
+            ''')
+        radar_chart('Radar Chart', df)
 
-    with st.expander('Scatter Chart Help'):
-        st.help(scatter_chart)
-    scatter_chart('Scatter Chart', df)
+        with st.expander('Mixed Chart Help'):
+            st.help(mixed_chart)
+            st.code('''
+            mixed_chart('Mixed Chart', df)
+            ''')
+        mixed_chart('Mixed Chart', df, plot_types={
+            'Google':'line',
+            'Apple':'column',
+            'Microsoft':'area'
+        })
 
-    with st.expander('Polar Area Chart Help'):
-        st.help(polar_area_chart)
-    polar_area_chart('Polar Area Chart', df)
+        with st.expander('Scatter Chart Help'):
+            st.help(scatter_chart)
+            st.code('''
+            scatter_chart('Scatter Chart', df)
+            ''')
+        scatter_chart('Scatter Chart', df)
 
-    with st.expander('Radial Bar Chart Help'):
-        st.help(radial_bar_chart)
-    radial_bar_chart('Radial Bar Chart', df)
+        with st.expander('Polar Area Chart Help'):
+            st.help(polar_area_chart)
+            st.code('''
+            polar_area_chart('Polar Area Chart', df)
+            ''')
+        polar_area_chart('Polar Area Chart', df)
+
+        with st.expander('Radial Bar Chart Help'):
+            st.help(radial_bar_chart)
+            st.code('''
+            radial_bar_chart('Radial Bar Chart', df)
+            ''')
+        radial_bar_chart('Radial Bar Chart', df)
 
